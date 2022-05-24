c45b3b07d4f64b37a8165fdb8e53f818c1fca096
etienne <etiennerodriguez3@gmail.com>
2022-05-24 18:23:57 -0400

    password will fit any criteria given, however the length will be added to itself for every prompt chosen

--- js/script.js
+++ js/script.js
@@ -31,10 +31,14 @@ var generatePassword2 = function() {
   window.passwordLowercase = window.confirm("Would you like to include lowercase letters?")
 if (passwordLowercase) {
   window.alert("Your password has lowercase letters.")
+  let lower = true;  
+  window.lower = "abcdefghijklmnopqrstuvwxyz";
+  
   generatePassword3();
 }
 else {
 window.alert("Your password has no lowercase letters.")
+let lower = false;
 generatePassword3();
 }
 }
@@ -43,10 +47,13 @@ var generatePassword3 = function() {
 window.passwordUppercase = window.confirm("Would you like to include uppercase letters?")
 if (passwordUppercase) {
   window.alert("Your password has uppercase letters.")
+  let upper = true;
+  window.upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   generatePassword4();
 }
 else {
   window.alert("Your password has no uppercase letters.")
+  let upper = false;
   generatePassword4();
 }
 }
@@ -55,40 +62,59 @@ var generatePassword4 = function() {
   window.passwordNumeric = window.confirm("Would you like to include numbers?")
   if (passwordNumeric) {
     window.alert("Your password will have numbers.")
+    let numeric = true;
+    window.numeric = "1234567890";
     generatePassword5();
   }
   else {
     window.alert("Your password will not have numbers.")
+    let numeric = false;
     generatePassword5();
   }
 }
 
 var generatePassword5 = function() {
   window.passwordSpecial = window.confirm("Would you like to include special characters?")
-  if (passwordSpecial) {
+
+ if (!passwordLowercase && !passwordUppercase && !passwordNumeric && !passwordSpecial) {
+  window.alert("You must select at least one option in order to generate a password.")
+  generatePassword2();
+ }
+ if (passwordSpecial) {
     window.alert("Your password will have special characters.")
+    let special = true;
+    window.special = "!@#$%^&*()";
     writePassword();
   }
   else {
     window.alert("Your password will not have special characters.")
+    let special = false;
     writePassword();
   }
+
 }
 
 var password = document.getElementById("password");
 function writePassword() {
-  window.alert("hi")
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numeric = "1234567890";
-var special = "!@#$%^&*()";
 var password = "";
-  
-for (var i = 0; i <= passwordLength; i++){
+for (var i = 0; i < passwordLength; i++){
   if (passwordLowercase){
 var randomNumber = Math.floor(Math.random() * lower.length);
   password += lower.substring(randomNumber, randomNumber +1);
   }
+  if (passwordUppercase) {
+    var randomNumber = Math.floor(Math.random() * upper.length);
+    password += upper.substring(randomNumber, randomNumber +1);
+  }
+  if (passwordNumeric) {
+    var randomNumber = Math.floor(Math.random() * numeric.length);
+    password += numeric.substring(randomNumber, randomNumber +1);
+  }
+  if (passwordSpecial) {
+    var randomNumber = Math.floor(Math.random() * special.length);
+    password += special.substring(randomNumber, randomNumber +1);
+  }
+  
 document.getElementById("password").value = password;
 }
 }
