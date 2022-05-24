0f39f1e0840be1b08a485e85f933c4a2e0a52919
etienne <etiennerodriguez3@gmail.com>
2022-05-24 13:28:36 -0400

    added all prompts. you can set your criteria, but no password generating yet

--- js/script.js
+++ js/script.js
@@ -11,28 +11,67 @@ window.alert("Hello! Please click the 'Generate Password' button to begin.");
 
 
 document.getElementById('generate').onclick = function() {
-generatePassword();
+generatePassword1();
 }
 
 
-var generatePassword = function() {
-var promptOne = window.prompt("How long would you like your password to be? It can be anywhere from 8 - 128 characters.")
-if (promptOne >= 8 && promptOne <= 128) {
-window.alert("Your password will have " + promptOne + " characters.");
+var generatePassword1 = function() {
+window.passwordLength = window.prompt("How long would you like your password to be? It can be anywhere from 8 - 128 characters.")
+if (passwordLength >= 8 && passwordLength <= 128) {
+window.alert("Your password will have " + passwordLength + " characters.");
+generatePassword2();
 }
 else {
 window.alert ("You have not chosen a correct response!")
-generatePassword();
+generatePassword1();
 }
 }
 
+var generatePassword2 = function() {
+  window.passwordLowercase = window.confirm("Would you like to include lowercase letters?")
+if (passwordLowercase) {
+  window.alert("Your password has lowercase letters.")
+  generatePassword3();
+}
+else {
+window.alert("Your password has no lowercase letters.")
+generatePassword3();
+}
+}
 
+var generatePassword3 = function() {
+window.passwordUppercase = window.confirm("Would you like to include uppercase letters?")
+if (passwordUppercase) {
+  window.alert("Your password has uppercase letters.")
+  generatePassword4();
+}
+else {
+  window.alert("Your password has no uppercase letters.")
+  generatePassword4();
+}
+}
 
+var generatePassword4 = function() {
+  window.passwordNumeric = window.confirm("Would you like to include numbers?")
+  if (passwordNumeric) {
+    window.alert("Your password will have numbers.")
+    generatePassword5();
+  }
+  else {
+    window.alert("Your password will not have numbers.")
+    generatePassword5();
+  }
+}
 
-
-
-
-
+var generatePassword5 = function() {
+  window.passwordSpecial = window.confirm("Would you like to include special characters?")
+  if (passwordSpecial) {
+    window.alert("Your password will have special characters.")
+  }
+  else {
+    window.alert("Your password will not have special characters.")
+  }
+}
 
 
 
