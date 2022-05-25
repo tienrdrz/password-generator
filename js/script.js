434813fb4f753328baf9d9af98c9c92bf8b15c27
etienne <etiennerodriguez3@gmail.com>
2022-05-25 17:27:36 -0400

    added comments to explain my work + cleaned up the messy js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@
 // Assignment code here
 
 /* When I click on the button I should:
-See more than one window prompt,
-Each prompt will ask for different criteria,
-And depending on what you select your password will contain that,
-And not use whatever you don't select.
+
+- See more than one window prompt,
+- Each prompt will ask for different criteria,
+- And depending on what you select your password will contain that,
+- And not use whatever you don't select.
+
 */ 
 window.alert("Hello! Please click the 'Generate Password' button to begin.");
 
@@ -14,7 +16,11 @@ document.getElementById('generate').onclick = function() {
 generatePassword1();
 }
 
-
+/* 
+Here I added the first prompt asking how long it will be, 
+in order to check for a correct response I set a min of 8
+and a max of 128
+*/
 var generatePassword1 = function() {
 window.passwordLength = window.prompt("How long would you like your password to be? It can be anywhere from 8 - 128 characters.")
 if (passwordLength >= 8 && passwordLength <= 128) {
@@ -83,7 +89,7 @@ var generatePassword5 = function() {
  if (passwordSpecial) {
     window.alert("Your password will have special characters.")
     let special = true;
-    window.special = "!@#$%^&*()";
+    window.special = "!@#$%^&*()-+";
     writePassword();
   }
   else {
@@ -93,6 +99,14 @@ var generatePassword5 = function() {
   }
 
 }
+
+/*
+Most of the prompts are the same thing copied with different questions,
+but in order to simplify my work I decided it would be best to create
+individual functions for each prompt, and make a global variable in each
+using the .window command before giving the variable a name.
+*/
+
 
 var password = document.getElementById("password");
 function writePassword() {
@@ -121,22 +135,9 @@ document.getElementById("password").value = finalPassword;
 }
 }
 
-
-// // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   window.alert ("HI")
-
-//   passwordText.value = password;
-// }
-//   var min = 8;
-//   var max = 128;
-// // Add event listener to generate button
-// if (generateBtn.addEventListener("click", writePassword)) {
-//   writePassword();
-// }
+/* 
+I ran into a problem where my code would double depending on how many
+different prompts were active, so in order to avoid that I found the 
+'slice' feature that will allow the function.writePassword to perform
+correctly without any bugs.
+*/
